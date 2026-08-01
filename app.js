515214cd60382072286a0da42d35038b11060de0
gsrakshi2-ly <g.s.rakshikkhasree2012@gmail.com>
2026-08-02 00:43:04 +0530

    dashboard

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const button = document.getElementById("startButton");
+function updateClock() {
 
-button.addEventListener("click", () => {
+    const now = new Date();
 
-    alert("🌸 Welcome to Bloom Studio!");
+    // Time
+    const time = now.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
 
-});
+    // Date
+    const date = now.toLocaleDateString([], {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
+
+    document.getElementById("clock").textContent = "🕒 " + time;
+
+    document.getElementById("date").textContent = "📅 " + date;
+
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
